c47294073635a0a333f8301e74195c27507ecf52
Fredy <Leroysane0@hotmail.com>
2025-01-17 11:51:29 +0100

    Creo el Proyecto Spring con las dependencias de msql , Jpa y web

---
+++ demo/src/main/java/ProIntermodular/demo/MercaproApplication.java
@@ -0,0 +1,13 @@
+package ProIntermodular.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaproApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/ProIntermodular/demo/MercaproApplicationTests.java
@@ -0,0 +1,13 @@
+package ProIntermodular.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
